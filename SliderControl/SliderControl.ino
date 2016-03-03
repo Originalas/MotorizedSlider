c3efbd37695dfd18eb8a6852f292652c98ac88d0
Originalas <originalas@gmail.com>
2016-03-02 17:37:54 -0800

    3 button control

--- SliderControl/SliderControl.ino
+++ SliderControl/SliderControl.ino
@@ -6,13 +6,16 @@
   //buttons ///////////////////////////////////////////////////////////////
 int inPin = 12;         // the number of the input pin
 int inPin2 = 11;  
+int inPin3 = 10; 
 int outPin = 13;       // the number of the output pin
 
 int state = HIGH;      // the current state of the output pin
 int reading;  // the current reading from the input pin
 int button2; 
+int button3; 
 int previous = LOW;    // the previous reading from the input pin
 int previous2 = LOW;    // the previous reading from the input pin
+int previous3 = LOW;    // the previous reading from the input pin
 
 // the follow variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
@@ -23,10 +26,12 @@ long debounce = 200;   // the debounce time, increase if the output flickers
 
 //motor
 #include <Stepper.h>
-int oneway;                      // counter for steps
-int onerev = 5000;               // number of steps each direction (edit this for your particular motor)
+unsigned int oneway = 1;                      // counter for steps
+unsigned int onerev = 15400;               // limit turns by slider length
 int microSecDelay = 100;          // delay between steps and speed of the motor  (about as fast as the system can react, higher number = slower)
 int microSecDelayStepSpeed = 1000; //turn speed delay, higher -> slower turn
+int speed1 = 1000; //turn speed mode 1 / normal
+int speed2 = 10000; //turn speed mode 2 / slow timelapse
 int dirPin = 8;                  // output pin for stepper motor direction
 int stepPin = 9;                 // output pin for the pin used to step the motor 
 int turn = LOW;
@@ -34,8 +39,9 @@ int turn = LOW;
 void setup() {
   //buttons ///////////////////////////////////////////////////////////////
   
-  pinMode(inPin, INPUT_PULLUP);
+  pinMode(inPin, INPUT_PULLUP); //Assign button pin, turn pullup resitor on
   pinMode(inPin2, INPUT_PULLUP);
+  pinMode(inPin3, INPUT_PULLUP);
   pinMode(outPin, OUTPUT);
 
   //buttons ///////////////////////////////////////////////////////////////
@@ -57,10 +63,12 @@ void loop()
   
   reading = digitalRead(inPin);
   button2 = digitalRead(inPin2);
+  button3 = digitalRead(inPin3);
 
   // if the input just went from LOW and HIGH and we've waited long enough
   // to ignore any noise on the circuit, toggle the output pin and remember
   // the time
+  // turn direction button
   if (reading == HIGH && previous == LOW && millis() - time > debounce) {
     if (state == HIGH)
       state = LOW;
@@ -75,17 +83,33 @@ void loop()
    //  turn = HIGH;
  // } else {turn = LOW;}
 
-
+  //start/stop button
    if (button2 == HIGH && previous2 == LOW && millis() - time > debounce) {
     if (turn == HIGH)
-      turn = LOW;
-    else
+  {   
+    turn = LOW;
+    oneway=1; //reset step count
+  }else{
       turn = HIGH;
-
+  }
     time = millis();    
   }
 
     previous2 = button2;
+
+  //speed button
+   if (button3 == HIGH && previous3 == LOW && millis() - time > debounce) {
+    if (microSecDelayStepSpeed == speed1)
+  {   
+      microSecDelayStepSpeed = speed2;
+   // oneway=1; //reset step count
+  }else{
+      microSecDelayStepSpeed = speed1;
+  }
+    time = millis();    
+  }
+
+    previous3 = button3;
   
   digitalWrite(outPin, state);
   digitalWrite(dirPin, state);
@@ -97,14 +121,16 @@ void loop()
 
   //motor ///////////////////////////////////////////////////////////////
 
-  if (turn == HIGH){ 
+  if (turn == HIGH && oneway < onerev){ 
     digitalWrite(stepPin, HIGH);       // Step motor
     delayMicroseconds(microSecDelay);  // Wait microseconds
     digitalWrite(stepPin, LOW);        // Step motor
     delayMicroseconds(microSecDelay);  // Wait microseconds
     delayMicroseconds(microSecDelayStepSpeed);  // Waint between steps - turn speed
+    oneway++; //count steps
   } else {
     delayMicroseconds(microSecDelayStepSpeed);  // Step speed  
+ 
     }
      
   //motor ///////////////////////////////////////////////////////////////
